cca22e2abca24efe640cfca0cb6dfd1ef33add13
NgeKaworu <NgeKaworu@163.com>
2020-10-12 16:23:55 +0800

    上周 昨天

--- src/pages/statistic/index.tsx
+++ src/pages/statistic/index.tsx
@@ -180,8 +180,16 @@ export default () => {
                 allowClear
                 showTime={{ format: "HH:mm" }}
                 ranges={{
-                  "今天": [moment(), moment()],
+                  "今天": [moment().startOf("day"), moment().endOf("day")],
+                  "昨天": [
+                    moment().add(-1, "day").startOf("day"),
+                    moment().add(-1, "day").endOf("day"),
+                  ],
                   "本周": [moment().startOf("week"), moment().endOf("week")],
+                  "上周": [
+                    moment().add(-1, "week").startOf("week"),
+                    moment().add(-1, "week").endOf("week"),
+                  ],
                   "本月": [moment().startOf("month"), moment().endOf("month")],
                   "今年": [moment().startOf("year"), moment().endOf("year")],
                 }}
