5b641d91f509a3acfaf8cd1ee4e543d32b55af10
fuRan <NgeKaworu@gmail.com>
2020-12-24 16:00:01 +0800

    cdn 加速

--- .umirc.ts
+++ .umirc.ts
@@ -28,4 +28,20 @@ export default defineConfig({
     loading: '@/Loading',
   },
   favicon: './assets/favicon.ico',
+  analyze: {
+    analyzerMode: 'server',
+    analyzerPort: 8888,
+    openAnalyzer: true,
+    // generate stats file while ANALYZE_DUMP exist
+    generateStatsFile: false,
+    statsFilename: 'stats.json',
+    logLevel: 'info',
+    defaultSizes: 'parsed', // stat  // gzip
+  },
+  externals: {
+    moment: 'moment',
+  },
+  scripts: [
+    'https://lib.baomitu.com/moment.js/latest/moment.min.js',
+  ]
 });
