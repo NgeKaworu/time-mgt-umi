5c6c6fa28f47737d9aef7420d8711624407fe0c0
fuRan <NgeKaworu@gmail.com>
2021-12-27 13:05:12 +0800

    feat:

--- .umirc.ts
+++ .umirc.ts
@@ -1,28 +1,17 @@
 import { defineConfig } from 'umi';
 import base from './.umirc.default';
+import routes from './routes';
 
 export default defineConfig({
   ...base,
   dva: {},
   title: '柳比歇夫时间管理法',
-  routes: [
-    {
-      path: '/',
-      component: '@/layouts/',
-      routes: [
-        { path: '/', redirect: '/record/' },
-        { path: '/record/', component: 'record' },
-        { path: '/statistic/', component: 'statistic' },
-        // { redirect: '/record/' },
-      ],
-    },
-  ],
+  routes,
   devServer: {
-    port: 8030,
+    port: 8051,
     proxy: {
       '/api/time-mgt': {
-        // target: 'https://api.furan.xyz/time-mgt',
-        target: 'http://localhost:8031',
+        target: 'http://localhost:8050',
         changeOrigin: true,
         pathRewrite: {
           '/api/time-mgt': '',
